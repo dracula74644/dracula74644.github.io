a02e4bb88e7e832db93433fadba8065005aa893c
DRACULA <38792325+dracula74644@users.noreply.github.com>
2025-07-04 15:00:53 +0530

    Update hey.cmd

--- hey.cmd
+++ hey.cmd
@@ -2,4 +2,4 @@
 set MSG=%*
 curl -H "Content-Type: application/json" -X POST ^
  -d "{\"content\":\"%MSG%\"}" ^
- "https://discord.com/api/webhooks/1308072993993265152/lJejN1Hyzp1-uGP_nCdIzP75SiljfFfjlwoIntJKUbHRH0RBcW-U1a_jrfY_NX_ekSg1"
+ "https://discord.com/api/webhooks/1390625756639531010/kb4O5FJ9BX8_VGgHkVyA8HrhLICAf8sGX7C-aIMi_36i3WBpQkmfjk6KNGbM9tbtZ8Mf"
